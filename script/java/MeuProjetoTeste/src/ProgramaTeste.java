72f19eaed5aff22ba340758760d4170e941ee99b
wertysopli <gustavo.stok@etec.sp.gov.br>
2018-10-08 22:35:10 -0300

    jopition e toper

---
+++ script/java/MeuProjetoTeste/src/ProgramaTeste.java
@@ -0,0 +1,8 @@
+
+public class ProgramaTeste {
+
+    public static void main(String[] args) {
+        System.out.println("Rubens");
+    }
+    
+}
---
+++ script/java/MeuProjetoTeste/src/ProgramaTesteComJanela.java
@@ -0,0 +1,8 @@
+import static javax.swing.JOptionPane.showMessageDialog;
+public class ProgramaTesteComJanela {
+
+    public static void main(String[] args) {
+        showMessageDialog(null, "Rubens");
+    }
+    
+}
